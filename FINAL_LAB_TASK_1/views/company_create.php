51e07a1c3738ece0a9b478123546ab0e0b40358a
nayeem99 <62529328+nayeem99@users.noreply.github.com>
2020-08-18 23:11:56 +0600

    insert

---
+++ FINAL_LAB_TASK_1/views/company_create.php
@@ -0,0 +1,54 @@
+<?php
+	require_once('../php/session_header.php');
+	if (isset($_GET['error'])) {
+		
+		if($_GET['error'] == 'db_error'){
+			echo "Something went wrong...please try again";
+		}
+	}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Add user</title>
+</head>
+<body>
+
+	<form action="../php/companyadd.php" method="post">
+		<fieldset>
+			<legend>Insert Company Information</legend>
+			<table>
+			<tr>
+				<td>ID</td>
+				<td><input type="text" name="id"></td>
+			</tr>
+			<tr>
+				<td>Name</td>
+				<td><input type="text" name="company_name"></td>
+			</tr>
+			<tr>
+				<td>Description</td>
+				<td><input type="text" name="profile_description"></td>
+			</tr>
+			<tr>
+				<td>Industry</td>
+				<td><input type="text" name="industry"></td>
+			</tr>
+			<tr>
+				<td>Website</td>
+				<td><input type="text" name="company_website"></td>
+			</tr>
+			<tr>
+				<td>Logo</td>
+				<td><input type="file" name="company_logo"></td>
+			</tr>
+			<tr>
+				<td><input type="submit" name="create" value="Add"></td>
+			</tr>
+		</table>
+		</fieldset>
+	</form>
+</body>
+</html>
---
+++ FINAL_LAB_TASK_1/views/company_info.php
@@ -0,0 +1,55 @@
+<?php
+	require_once('../php/session_header.php');
+	require_once('../service/userService.php');
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<a href="home.php">Back</a> |
+	<a href="../php/logout.php">Logout</a> 
+	
+	<h3>Company Information</h3>
+
+	<a href="../views/company_create.php">Add_Company_INFO</a><br><br>
+
+	<table border="1">
+		<tr>
+			<td>ID</td>
+			<td>Company Name</td>
+			<td>Profile Description</td>
+			<td>Industry</td>
+			<td>Company Website</td>
+			<td>Company Logo</td>
+			<td>User Account Id</td>
+			<td>Action</td>
+		</tr>
+
+		<?php  
+			$users = getAllCompany();
+			for ($i=0; $i != count($users); $i++) {  ?>
+		<tr>
+			<td><?=$users[$i]['id']?></td>
+			<td><?=$users[$i]['company_name']?></td>
+			<td><?=$users[$i]['profile_description']?></td>
+			<td><?=$users[$i]['industry']?></td>
+			<td><?=$users[$i]['company_website']?></td>
+			<td><img src=<?='"'.$users[$i]['company_logo'].'"'?>height="200px" width="200px"></td>
+			<td><?=$users[$i]['user_account_id']?></td>
+			<td>
+				<a href="edit.php?id=<?=$users[$i]['id']?>">Edit</a> |
+				<a href="delete.php?id=<?=$users[$i]['id']?>">Delete</a> 
+				
+			</td>
+		</tr>
+
+		<?php } ?>
+		
+	</table>
+
+</body>
+</html>
