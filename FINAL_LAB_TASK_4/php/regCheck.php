6774f4e0391f9055351150e7fa9311e9e1c6fa52
nayeem99 <62529328+nayeem99@users.noreply.github.com>
2020-09-01 09:53:05 +0600

    Update regCheck.php

--- FINAL_LAB_TASK_4/php/regCheck.php
+++ FINAL_LAB_TASK_4/php/regCheck.php
@@ -1,33 +1,18 @@
 <?php 
-	session_start();
-	require_once('../service/userService.php');
-
-	if(isset($_POST['submit'])){
-
-		$username 	= $_POST['username'];
-		$password 	= $_POST['password'];
-		$email 		= $_POST['email'];
-
-		if(empty($username) || empty($password) || empty($email)){
-			header('location: ../views/register.php?error=null_value');
-		}else{
-
-			$user = [
+<?php 
+require_once('../service/userService.php');
+$username=$_POST['name'];
+$password=$_POST['password'];
+$email=$_POST['email'];
+$user = [
 				'username'=> $username,
 				'password'=> $password,
 				'email'=> $email
 			];
-
-			$status = insert($user);
-
-			if($status){
-				header('location: ../views/login.php?success=registration_done');
-			}else{
-				header('location: ../views/register.php?error=db_error');
-			}
-		}
-	}
-
+			
+$status = insert($user);
+echo $status;
+?>
 
 
 ?>
