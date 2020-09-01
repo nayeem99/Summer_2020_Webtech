e5f7e8c3eaca15719915ce38bdd432dd5b625fc5
nayeem99 <62529328+nayeem99@users.noreply.github.com>
2020-09-01 09:27:42 +0600

    Update register.js

--- FINAL_LAB_TASK_4/views/register.js
+++ FINAL_LAB_TASK_4/views/register.js
@@ -3,7 +3,7 @@ var name = document.getElementById('name').value;
 
 function nempty()
 {
-	if(name="")
+	if(name=="")
 	{
 		document.getElementById("nameMsg").innerHTML="field can't be empty";
 		nameval=false;
@@ -21,3 +21,60 @@ function nameRemover()
     
 }
 
+var emailval=false;
+var email = document.getElementById('email').value;
+
+function emailempty()
+{
+	if(email=="")
+	{
+		document.getElementById("emailMsg").innerHTML="field can't be empty";
+		emailval=false;
+	}
+	else{
+		nameval= true;
+	}
+
+}
+
+function emailRemover()
+{
+    document.getElementById('emailMsg').innerHTML = "";
+    
+}
+
+var passval=false;
+var password = document.getElementById('password').value;
+
+function passempty()
+{
+	if(password=="")
+	{
+		document.getElementById("passMsg").innerHTML="field can't be empty";
+		passval=false;
+	}
+	else{
+		passval= true;
+	}
+}
+
+
+
+function passRemover()
+{
+    document.getElementById('passMsg').innerHTML = "";
+    
+}
+
+function f1(){
+			document.getElementsByTagNames('a')[0].style.display='inline';
+		}
+
+if(nameval && emailval && passval )
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
